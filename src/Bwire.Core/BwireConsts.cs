0a4d3115ffbdc5d46208bf4e2a2dcde824a349fa
abdok1234 <abdok1234@gmail.com>
2022-05-12 18:58:57 +0300

    initial

---
+++ src/Bwire.Core/BwireConsts.cs
@@ -0,0 +1,20 @@
+﻿using Bwire.Debugging;
+
+namespace Bwire
+{
+    public class BwireConsts
+    {
+        public const string LocalizationSourceName = "Bwire";
+
+        public const string ConnectionStringName = "Default";
+
+        public const bool MultiTenancyEnabled = true;
+
+
+        /// <summary>
+        /// Default pass phrase for SimpleStringCipher decrypt/encrypt operations
+        /// </summary>
+        public static readonly string DefaultPassPhrase =
+            DebugHelper.IsDebug ? "gsKxGZ012HLL3MI5" : "1f530bffa2994615b1174535199b09b7";
+    }
+}
---
+++ src/Bwire.EntityFrameworkCore/EntityFrameworkCore/BwireDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using Bwire.Authorization.Roles;
+using Bwire.Authorization.Users;
+using Bwire.MultiTenancy;
+
+namespace Bwire.EntityFrameworkCore
+{
+    public class BwireDbContext : AbpZeroDbContext<Tenant, Role, User, BwireDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public BwireDbContext(DbContextOptions<BwireDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
